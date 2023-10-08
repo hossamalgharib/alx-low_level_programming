ab0a8d7496c007239fc614c6a2aae699f26c1ab4
‘hossamalgharib’ <hossam.elghareeb100@gmail.com>
2023-10-07 23:23:20 -0700

    commit-3

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - it's entry point
+ *
+ *  and it's  booooring
+ *
+ *  Return: Always 0
+ */
+
 int main(void)
 {
 	int i;
@@ -8,10 +16,10 @@ int main(void)
 	long int li;
 	long long int lli;
 
-	printf("Size of an int: %lu.\n", (unsigned long)sizeof(i));
-       	printf("Size of an char: %lu.\n", (unsigned long)sizeof(c));
-	printf("Size of an float: %lu.\n", (unsigned long)sizeof(f));
-	printf("Size of an long int: %lu.\n", (unsigned long)sizeof(li));
-	printf("Size of an long long int: %lu.\n", (unsigned long)sizeof(lli));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+       	printf("Size of an char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("Size of an long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+	printf("Size of an long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
 	return(0);
 }
